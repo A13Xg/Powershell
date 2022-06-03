f742c9568b0e936bdff84f53cf76754c32611153
Alexander Goodman <38967711+A13Xg@users.noreply.github.com>
2022-06-03 09:54:23 -0700

    Update

--- WindowsHardeningSetup/CAT1.ps1
+++ WindowsHardeningSetup/CAT1.ps1
@@ -11,13 +11,16 @@ A13Xg Industries
 # This code is designed to automate a majority of Windows10 hardening.
 # Most of the controls are set based on common 'Best Practices' or STIGs refrenced by DISA.
 
+#This portion will patch all the CAT1 Vulnerabilites.
+
+
 #region \/ GLOBAL VARIABLES \/
 
 $global:logLocation = "C:\WinHardeningLog.txt"
 $global:workingDir = "C:\Utils\Scripts\Win10Hardening\"
 $global:currentVersion = "Ver 0.1"
-$host.ui.RawUI.WindowTitle = "Windows10 Hardening"
-[int]$taskTotal = 10
+$host.ui.RawUI.WindowTitle = "Windows10 Hardening -- CAT1"
+[int]$taskTotal = 5
 #endregion /\ GLOBAL VARIABLES /\
 
 #region \/ FUNCTIONS \/
@@ -54,10 +57,6 @@ function WriteCmdLine {
        # Colors "Black","Blue","Cyan","DarkBlue","DarkCyan","DarkGray","DarkGreen","DarkMagenta","DarkRed","DarkYellow","Gray","Green","Magenta","Red","White","Yellow"
        Write-Host $Message -ForegroundColor $Color 
     }
-#// Function to write blank line
-function WriteBlank {
-    WriteCmdLine -Message "`n" -Color "YELLOW"
-}
 #// Function to print date in specified format
 function DateStamp {
         Get-Date -Format "MM/dd/yyyy-HH:mm"
@@ -71,31 +70,17 @@ function Loginator {
         $Date = (DateStamp)
         "[$Date] ~   $Message" | Out-File -FilePath $Path -Append
     }
-#// Function To Run a Process
-    function ProcRun {
-        param (
-            [string] $Name,
-            [string] $CodeAction
-        )
-        [int]$taskVal = $taskVal + 1
-        WriteCmdLine -Message "Starting Action: $Name" -Color "Blue"
-        WriteCmdLine -Message "Task $taskVal of $taskTotal"
-        Start-Process powershell.exe $CodeAction
-        Loginator -Message "Executed: $CodeAction"
-    }
+#// Function to generalize patching
+function ProcRun {
+    param (
+        [string] $Name,
+        [string] $CodeAction
+    )
+    [int]$taskVal = $taskVal + 1
+    WriteCmdLine -Message "Starting Action: $Name" -Color "Blue"
+    WriteCmdLine -Message "Task $taskVal of $taskTotal"
+}
 #endregion /\ FUNCTIONS /\
 
-#// Start script and initiate Log file.
-WriteCmdLine -Message "ENSURE SCRIPT IS RUNNING AS ADMINISTRATOR!" -Color "RED"
-PAUSE
-WriteCmdLine -Message "Initializing Script" -Color "YELLOW"
-Loginator -Message "Script Initiated by $env:USERNAME"
-WriteBlank
-WriteCmdLine -Message "Beginning Process..." -Color "BLUE"
-WriteBlank
-ProcRun -Name test -CodeAction notepad.exe
-
-
-
-WriteCmdLine -Message "Process Completed Successfully!" -Color "Green"
+Loginator -Message "CAT1 Process Launched"
 Pause
